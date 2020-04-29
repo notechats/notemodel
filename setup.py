b0bb699db11bbff27308e9737ecabdd581fa2b63
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-04-30 00:12:11 +0800

    add

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 install_requires = ['numpy']
 
 setup(name='notemodel',
-      version='0.0.1',
+      version='0.1.0',
       description='notemodel',
       author='niuliangtao',
       author_email='1007530194@qq.com',
